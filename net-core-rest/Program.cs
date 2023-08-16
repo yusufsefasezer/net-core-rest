08ca147d4a0cefc7c178c4bb09feca93db240667
Yusuf Sezer <yusufsezer@mail.com>
2023-08-16 21:46:39 +0300

    upgraded .NET Core v2.2 to .NET v7.0

--- net-core-rest/Program.cs
+++ net-core-rest/Program.cs
@@ -1,26 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-namespace net_core_rest
-{
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<net_core_rest.Data.ContactDbContext>(options =>
+{
+    options.UseInMemoryDatabase("Contact");
+});
+
+// Register the Swagger generator, defining 1 or more Swagger documents
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = ".NET Core Contact",
+        Description = ".NET Core Contact API",
+        TermsOfService = new Uri("https://www.yusufsezer.com"),
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact { Name = "Yusuf SEZER", Email = "yusufsezer@mail.com", Url = new Uri("https://www.yusufsezer.com") },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(options =>
+{
+    options.DocumentTitle = ".NET Core Contact API";
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", ".NET Core Contact API");
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
